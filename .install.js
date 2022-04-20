4a3541e4c0c10d6af22cad7f36ad1ed7536bd300
Anton <58663761+0LEG0@users.noreply.github.com>
2022-04-20 12:10:26 +0300

    Postinstall for config files

---
+++ .install.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fs = require("fs");
+
+function copyConf(src, dst) {
+    fs.readdir(src, (err, configs) => {
+        if (err) return;
+        configs.forEach(confile => {
+            fs.access(dst + confile, (err) => {
+                if (err) {
+                    fs.copyFile(src + confile, dst + confile, (err) => {
+                        if (err)
+                            console.log("Failed copy config file", src + confile);
+                        else
+                            console.log("Copy config file", src + confile + " -> " + dst + confile);
+                    });
+                } else {
+                    console.log("Keep old config file", dst + confile);
+                }
+            });
+        });
+    })
+}
+fs.readdir("node_modules", (err, node_modules) => {
+    if (err) return;
+    node_modules.forEach(module => {
+        if (module.startsWith(".")) return;
+        copyConf("node_modules/" + module + "/conf/", "conf/");
+    })
+})
